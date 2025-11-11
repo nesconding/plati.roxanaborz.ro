3c30a3616a11b049934119b28fee5363b1f4c96d
Andrei Vǎruț <andrei@syncron.app>
2025-11-11 20:00:48 +0100

    Disable devtools

--- src/client/components/utils/react-scan.tsx
+++ src/client/components/utils/react-scan.tsx
@@ -1,14 +1,14 @@
 'use client'
 
 import { type JSX, useEffect } from 'react'
-// import { scan } from 'react-scan'
-import { scan } from 'react-scan/all-environments'
+import { scan } from 'react-scan'
+// import { scan } from 'react-scan/all-environments'
 
 export function ReactScan(): JSX.Element | null {
   useEffect(() => {
     if (process.env.NODE_ENV !== 'development') return
     scan({
-      enabled: true
+      enabled: false
     })
   }, [])
 
--- src/client/lib/providers/devtools.tsx
+++ src/client/lib/providers/devtools.tsx
@@ -1,16 +1,16 @@
 'use client'
 
-import { TanStackDevtools } from '@tanstack/react-devtools'
-import { FormDevtoolsPlugin } from '@tanstack/react-form-devtools'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+// import { TanStackDevtools } from '@tanstack/react-devtools'
+// import { FormDevtoolsPlugin } from '@tanstack/react-form-devtools'
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 export function Devtools() {
   if (process.env.NODE_ENV !== 'development') return null
 
   return (
     <>
-      <ReactQueryDevtools buttonPosition='bottom-left' />
-      <TanStackDevtools plugins={[FormDevtoolsPlugin()]} />
+      {/* <ReactQueryDevtools buttonPosition='bottom-left' />
+      <TanStackDevtools plugins={[FormDevtoolsPlugin()]}  /> */}
     </>
   )
 }
