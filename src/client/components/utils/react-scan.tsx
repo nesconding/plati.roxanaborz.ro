16aab8eec4b94719656ddab0b0809b37d46c468f
Andrei Vǎruț <andrei@syncron.app>
2025-11-12 10:33:36 +0100

    Enable devtools in development mode

--- src/client/components/utils/react-scan.tsx
+++ src/client/components/utils/react-scan.tsx
@@ -1,14 +1,13 @@
 'use client'
 
 import { type JSX, useEffect } from 'react'
-import { scan } from 'react-scan'
-// import { scan } from 'react-scan/all-environments'
+import { scan } from 'react-scan/all-environments'
 
 export function ReactScan(): JSX.Element | null {
   useEffect(() => {
-    if (process.env.NODE_ENV !== 'development') return
+    if (process.env.NODE_ENV === 'production') return
     scan({
-      enabled: false
+      enabled: process.env.NODE_ENV === 'development'
     })
   }, [])
 
