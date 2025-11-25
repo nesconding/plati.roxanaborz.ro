c4fb6c4f1ab56ff37bd31a0424db4fd3bd408e6d
Andrei Vǎruț <andrei@syncron.app>
2025-11-25 23:15:35 +0100

    Remove updateOrderBillingDataProcedure and its references from contractsRouter to streamline contract management functionality.

--- src/server/trpc/router/public/contracts/router.ts
+++ src/server/trpc/router/public/contracts/router.ts
@@ -1,8 +1,6 @@
 import { createTRPCRouter } from '~/server/trpc/config'
 import { generateFilledContractProcedure } from '~/server/trpc/router/public/contracts/procedures/generate-filled-contract'
-import { updateOrderBillingDataProcedure } from '~/server/trpc/router/public/contracts/procedures/update-order-billing-data'
 
 export const contractsRouter = createTRPCRouter({
-  generateFilledContract: generateFilledContractProcedure,
-  updateOrderBillingData: updateOrderBillingDataProcedure
+  generateFilledContract: generateFilledContractProcedure
 })
